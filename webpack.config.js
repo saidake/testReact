669a49a758f027ca7906991455f276aa2f20dc1f
saidake <saidake@qq.com>
2021-02-28 14:35:48 +0800

    dv

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,18 @@ module.exports={
 	},
 	module:{
 		rules:[
-
+			{
+				test:/\.js$/,
+				exclude:/node_modules/,
+				use:{
+					loader:'babel-loader',
+					options:{
+						presets:[
+							['@babel/preset-env',{target:"defaults"}]
+						]
+					}
+				}
+			}
 		]
 	},
 	devServer:{
